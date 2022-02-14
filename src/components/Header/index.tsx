9f4f6e35f539cc5798e18f91846b2694c0d62dcd
Celso Farias <fariasoc@outlook.com>
2022-02-13 23:33:33 -0300

    Separando componentes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,8 @@
-import { Flex, Text, Input, Icon, Stack, HStack, Box, Avatar } from '@chakra-ui/react'
-import { RiNotificationLine, RiSearchLine, RiUserAddLine} from 'react-icons/ri'
-import { Logo } from './Header/Logo'
-import { NotificationsNav } from './Header/NotificationsNav'
-import { Profile } from './Header/Profile'
-import { SearchBox } from './Header/SearchBox'
+import { Flex } from '@chakra-ui/react'
+import { Logo } from './Logo'
+import { NotificationsNav } from './NotificationsNav'
+import { Profile } from './Profile'
+import { SearchBox } from './SearchBox'
 
 export function Header(){
   return (
