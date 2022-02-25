62636ead568060ce44b6ba4c12cc2a3ad9960f06
Celso Farias <fariasoc@outlook.com>
2022-02-25 07:03:05 -0300

    Componente de paginação

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -3,9 +3,10 @@ import { Button } from "@chakra-ui/react";
 interface PaginationItemProps {
   number: number;
   isCurrent?: boolean;
+  onPageChange: (page: number) => void;
 }
 
-export function PaginationItem({ isCurrent = false, number }: PaginationItemProps) {
+export function PaginationItem({ isCurrent = false, onPageChange, number }: PaginationItemProps) {
   if (isCurrent) {
     return (
       <Button size="sm" fontSize="xs" width="4" colorScheme="pink" disabled
@@ -28,6 +29,7 @@ export function PaginationItem({ isCurrent = false, number }: PaginationItemProp
       _hover={{
         bg: 'gray.500'
       }}
+      onClick={() => onPageChange(number)}
     >
       {number}
     </Button>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,10 +5,13 @@ import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import Link from "next/link";
 import { useUsers } from "../../services/hooks/useUsers";
+import { useState } from "react";
 
 export default function UserList() {
 
-  const { data, isLoading, isFetching, error } = useUsers()
+  const [page, setPage] = useState(1)
+
+  const { data, isLoading, isFetching, error } = useUsers(page)
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -83,7 +86,7 @@ export default function UserList() {
 
             <Tbody>
 
-              {data.map(user => {
+              {data.users.map(user => {
                 return(
                          <Tr key={user.id}>
                               <Td px={["4", "4", "6"]} >
@@ -112,7 +115,11 @@ export default function UserList() {
 
           </Table >
 
-    <Pagination />
+    <Pagination
+      totalCountOfRegisters={data.totalCount}
+      currentPage={page}
+      onPageChange={setPage}
+    />
 </>
 
           )
