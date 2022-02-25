49b58a1721a4a8ad57b37fadf2dbb8691496b01e
Celso Farias <fariasoc@outlook.com>
2022-02-24 23:04:55 -0300

    configurando o axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,17 +1,17 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
 import { Pagination } from "../../components/Pagination";
 import { useQuery } from 'react-query';
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import Link from "next/link";
-import { useEffect } from "react";
+import { api } from "../../services/api";
 
 export default function UserList() {
 
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
+    const { data } = await api.get('users')
+
 
     const users = data.users.map(user => {
       return {
@@ -27,7 +27,7 @@ export default function UserList() {
     })
     return users
   }, {
-    staleTime: 1000 * 5 // Durante 5 segundos, o estado será mantido em fresh e após os 5 segundos, ela se tornará stale
+    staleTime: 1000 * 5 // Durante 5 segundos, o estado será mantido em fresh e após os 5 segundos, os estados se tornaram obsoletos
   })
 
   const isWideVersion = useBreakpointValue({
@@ -59,6 +59,7 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center" >
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
             </Heading>
 
             <Link href="/users/create" passHref>
